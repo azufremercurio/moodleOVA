ac81e7604e25aa85250c10bffcd03c926e1b1b51
hector_h_hernandez_orozco <uq.hector@gmail.com>
2017-12-19 20:01:10 -0500

    Leer parametros GET con javascript se realiza el codigo que obtiene los parametros GET desde la URL con el fin de obtener el id del curso y poder hacer consultas ne la base de datos mendiante este parametro

--- esap/Resources/js/resources.js
+++ esap/Resources/js/resources.js
@@ -30,35 +30,39 @@ var loadm = {
 };
 
 function fxloadm() {
-//    $('.buttons-content').load('/esap/courses/seguridadInformatica/proteccionDatos/index.php?id=1');
+    var urlString = window.location.href;
+    var url = new URL(urlString);
+    var id = url.searchParams.get("id");
+    alert("xxxx " + id);
     
-    var btn1 = $('<a>');
-    var btn2 = $('<a>');
-    var btn3 = $('<a>');
-            
-    $(btn1).html('Protección de datos').addClass('courseUnity btn btn-primary')
-            .prop({href: '/esap/courses/seguridadInformatica/proteccionDatos/?id=1', disabled: true});
-    $(btn2).html('Encriptación de datos').addClass('courseUnity btn btn-primary')
-            .prop('href', '/esap/courses/seguridadInformatica/proteccionDatos/?id=2');
-    $(btn3).html('Analisis de vulnerabilidad').addClass('courseUnity btn btn-primary')
-            .prop('href', '/esap/courses/seguridadInformatica/proteccionDatos/?id=3');
-
-    var fxLoadm = loadm;
-    var data = {
-        url: '/esap/app.php/test',
-        data: {id: 120, msn: "nelson"},
-        type: 'post',
-        dataType: 'html',
-        callback: function(result) {
-            console.log(333, result);
-        }
-    };
+//    $('.buttons-content').load('/esap/courses/seguridadInformatica/proteccionDatos/index.php?id=1');
+//    var btn1 = $('<a>');
+//    var btn2 = $('<a>');
+//    var btn3 = $('<a>');
+//            
+//    $(btn1).html('Protección de datos').addClass('courseUnity btn btn-primary')
+//            .prop({href: '/esap/courses/seguridadInformatica/proteccionDatos/?id=1', disabled: true});
+//    $(btn2).html('Encriptación de datos').addClass('courseUnity btn btn-primary')
+//            .prop('href', '/esap/courses/seguridadInformatica/proteccionDatos/?id=2');
+//    $(btn3).html('Analisis de vulnerabilidad').addClass('courseUnity btn btn-primary')
+//            .prop('href', '/esap/courses/seguridadInformatica/proteccionDatos/?id=3');
+//
+//    var fxLoadm = loadm;
+//    var data = {
+//        url: '/esap/app.php/test',
+//        data: {id: 120, msn: "nelson"},
+//        type: 'post',
+//        dataType: 'html',
+//        callback: function(result) {
+//            console.log(333, result);
+//        }
+//    };
 
     //fxLoadm.ajax(data);
 
-    $('.buttons-content').append(btn1);
-    $('.buttons-content').append(btn2);
-    $('.buttons-content').append(btn3);
+//    $('.buttons-content').append(btn1);
+//    $('.buttons-content').append(btn2);
+//    $('.buttons-content').append(btn3);
 }
 
 $(document).ready(function () {
